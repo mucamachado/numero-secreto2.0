f3546baf9a9c8f265abf240f46dcea31cc27a94f
Murilo Machado de Paula <mdepaula5080@gmail.com>
2025-09-13 17:50:34 -0300

    organizar exercicios em pastas

---
+++ exercicios/ex_003.js
@@ -0,0 +1,12 @@
+let listaGenerica = [];
+
+let linguagensDeProgramação = ['Javascript', 'C', 'C++'];
+linguagensDeProgramação.push('Java', 'Kotlin','Python' );
+
+let whatAmI = ['im', 'st8', 'asl'];
+
+console.log(listaGenerica);
+console.log(linguagensDeProgramação);
+console.log(whatAmI[0]);
+console.log(whatAmI[1]);
+console.log(whatAmI[2]);
